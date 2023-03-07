83502e37ed08d3e13b2a95429f7a2f24d0bf9e42
caiosabarros <caiohenriquesa1@gmail.com>
2023-03-06 22:39:05 -0300

    changing airdrop contract to a 1155

---
+++ contracts/Merkle.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract Merkle is ERC1155 {
+	
+    bytes32 root;
+    mapping(address => bool) claimed;
+
+    constructor(bytes32 _root) ERC1155("https://game.example/api/item/{id}.json") {
+        root = _root;
+    }
+
+    function airdrop(bytes32[] calldata _proof, address _accountToSendTokensTo) external {
+        bool isEligible = MerkleProof.verify(_proof, root, keccak256(abi.encodePacked(_accountToSendTokensTo)));
+        require(isEligible, "not eligible");
+        require(claimed[_accountToSendTokensTo] == false, "already claimed");
+        claimed[_accountToSendTokensTo] = true;
+        _mint(_accountToSendTokensTo, 1, 1, "");
+    }
+
+    
+
+}
+
--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 const { MerkleTree } = require('merkletreejs');
-const Token = artifacts.require("Token");
+const Merkle = artifacts.require("Merkle");
 
-contract("Token", function (accounts) {
+contract("Merkle", function (accounts) {
 	let eligible = accounts.slice(0, 9); // accounts indexes 0 to 9
 	console.log("eligible", eligible[0]);
 	let nonEligible = accounts.slice(10, 19); // accounts indexes 10 to 19
 	console.log("non-eligible", nonEligible[0]);
-	let tree, token;
+	let tree, merkle;
 
 	_buildMerkleTree = () => {
 		const leaves = eligible.map(leaf => web3.utils.keccak256(leaf));
@@ -14,37 +14,37 @@ contract("Token", function (accounts) {
 	}
 
 	_deployToken = async () => {
-		return await Token.new(tree.getHexRoot());
+		return await Merkle.new(tree.getHexRoot());
 	}
 
 	beforeEach(async () => {
 		tree = _buildMerkleTree();
-		token = await _deployToken();
+		merkle = await _deployToken();
 	});
 
 	it("claims the airdrop to an eligible address", async function () {
-		const balan = await token.balanceOf(eligible[1]);
+		const balan = await merkle.balanceOf(eligible[1], 1);
 		console.log("initial", balan.toString());
 		const leaf = web3.utils.keccak256(eligible[1]);
 		const proof = tree.getHexProof(leaf);
 
-		await token.airdrop(proof, eligible[1],{from: eligible[0]});
-		const balance = await token.balanceOf(eligible[1]);
+		await merkle.airdrop(proof, eligible[1],{from: eligible[0]});
+		const balance = await merkle.balanceOf(eligible[1], 1);
 		console.log(balance.toString());
-		assert.equal(balance, web3.utils.toWei("1000"));
+		assert.equal(balance, 1);
 	});
 
 	it("claims the airdrop to all eligible addresses", async function () {
 		for (let index = 0; index < eligible.length; index++) {
-			const balan = await token.balanceOf(eligible[index]);
+			const balan = await merkle.balanceOf(eligible[index], 1);
 			console.log("initial", balan.toString());
 			const leaf = web3.utils.keccak256(eligible[index]);
 			const proof = tree.getHexProof(leaf);
 	
-			await token.airdrop(proof, eligible[index],{from: nonEligible[0]});
-			const balance = await token.balanceOf(eligible[index]);
+			await merkle.airdrop(proof, eligible[index],{from: nonEligible[0]});
+			const balance = await merkle.balanceOf(eligible[index], 1);
 			console.log(balance.toString());
-			assert.equal(balance, web3.utils.toWei("1000"));
+			assert.equal(balance, 1);
 		}
 	});
 
@@ -53,7 +53,7 @@ contract("Token", function (accounts) {
 		const proof = tree.getHexProof(leaf);
 
 		try {
-			await token.airdrop(proof, nonEligible[1], {from: nonEligible[0]});
+			await merkle.airdrop(proof, nonEligible[1], {from: nonEligible[0]});
 			assert.fail();
 		} catch (e) {
 			assert.equal(e.message, "VM Exception while processing transaction: reverted with reason string 'not eligible'");
@@ -64,13 +64,18 @@ contract("Token", function (accounts) {
 		const leaf = web3.utils.keccak256(eligible[1]);
 		const proof = tree.getHexProof(leaf);
 
-		await token.airdrop(proof, eligible[1], {from: eligible[0]});
+		await merkle.airdrop(proof, eligible[1], {from: eligible[0]});
 
 		try {
-			await token.airdrop(proof, eligible[1], {from: eligible[0]});
+			await merkle.airdrop(proof, eligible[1], {from: eligible[0]});
 			assert.fail();
 		} catch (e) {
 			assert.equal(e.message, "VM Exception while processing transaction: reverted with reason string 'already claimed'");
 		}
+	});
+
+	it("prints the uri of the NFT airdropped", async function() {
+		const uri = await merkle.uri(1);
+		console.log(uri);
 	});
 });
